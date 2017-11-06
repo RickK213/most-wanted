a4eb9c5d9b7a7b3f48a721ed93a23a95caf12df2
Rick K <rkippert@att.net>
2017-11-06 10:45:54 -0600

    Added searchByName function call

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function app(people){
   var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
   switch(searchType){
     case 'yes':
-    // TODO: search by name
+      searchByName();
     break;
     case 'no':
     searchByTraits(people);
